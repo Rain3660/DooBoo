4fc786533c134bab750f164be4659e204cf69525
amuser11 <amuser11@gmail.com>
2021-07-22 12:32:06 +0900

    product 내용 삭제

--- src/main/java/com/koreait/dooboo/product/dao/ProductDAO.java
+++ src/main/java/com/koreait/dooboo/product/dao/ProductDAO.java
@@ -1,30 +1,9 @@
 package com.koreait.dooboo.product.dao;
 
-import java.util.List;
-
 import org.apache.ibatis.annotations.Mapper;
-
-import com.koreait.dooboo.communityboard.dto.CommunityBoardDTO;
-import com.koreait.dooboo.product.dto.ProductDTO;
-import com.koreait.dooboo.product.dto.ProductimageDTO;
 
 @Mapper
 public interface ProductDAO {
 	
-	// 상품을 판매등록 한다.
-	public int insertProduct(ProductDTO params);
-	
-	public ProductDTO selectProductDetail(long idx);
-	
-	public int updateProduct(ProductDTO params);
-	
-	public int deleteProduct(long idx);
 
-	public List<ProductDTO> selectProductList(ProductDTO productDTO);
-	
-	public int selectProductTotalCount(ProductDTO productDTO);
-
-	public int updateHit(long productNo);
-	
-	public int insertProductImage(ProductimageDTO productimageDTO);
 }
